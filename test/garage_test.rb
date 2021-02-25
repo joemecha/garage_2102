44feb3b82ca08c91b6723d3bddbc0dae8f6c578e
joemecha <jwmecha@gmail.com>
2021-02-25 10:46:48 -0600

    Add first garage tests

---
+++ test/garage_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/car'
+require './lib/owner'
+require './lib/garage'
+
+class GarageTest < Minitest::Test
+  def setup
+    @garage = Garage.new("Totally Safe Parking")
+
+    @owner_1 = Owner.new('Regina George', 'Heiress')
+    @owner_1.buy('1967 Green Ford Mustang')
+    @owner_1.buy('2001 Blue Ford Escape')
+
+    @owner_2 = Owner.new('Glen Coco', 'Unknown')
+    @owner_2.buy('1963 Green Chevrolet Corvette')
+    @owner_2.buy('2020 Silver Volvo XC90')
+  end
+
+  def test_it_exists
+    assert_instance_of Garage, @garage
+  end
+
+  def test_it_has_attribute
+    assert_equal "Totally Safe Parking", @garage.name
+  end
+end
